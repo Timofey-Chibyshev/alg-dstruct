bd3b505e942f01efd18ee3dac7c98f5598748274
tima <xjiscjcjd@inbox.ru>
2022-05-03 11:36:43 +0300

    first commit

---
+++ 3-ChibyshevTimofey-J20/HashTable.c
@@ -0,0 +1,141 @@
+#include "HashTable.h"
+
+unsigned int Hash(const char* s)
+{
+    unsigned int h = 0, g;
+    while (*s) {
+        h = (h << 4) + *s++;
+        g = h & 0xf0000000L;
+        if (g)
+        {
+            h ^= g >> 24;
+        }
+        h &= ~g;
+    }
+    return h % TABLE_SIZE;
+}
+
+static int LookUp(Node** table, const char* key)
+{
+    unsigned index = Hash(key);
+    const Node* cur = table[index];
+    while (cur && strcmp(cur->data, key))
+    {
+        cur = cur->next;
+    }
+    return cur != NULL;
+}
+
+int Insert(Node** table, char* key)
+{
+    if (!LookUp(table, key))
+    {
+        unsigned index = Hash(key);
+        Node* newNode = (Node*)malloc(sizeof * newNode);
+        if (!newNode)
+        {
+            return 0;
+        }
+        newNode->data = malloc((strlen(key) + 1) * sizeof(char));
+        if (!newNode->data)
+        {
+            return 0;
+        }
+        strcpy(newNode->data, key);
+        newNode->next = table[index];
+        table[index] = newNode;
+        return 1;
+    }
+    return 0;
+}
+
+int Delete(Node** table, char* key)
+{
+    if (LookUp(table, key))
+    {
+        unsigned int index = Hash(key);
+        Node* it = table[index];
+        Node* prev = NULL;
+        while (strcmp(it->data, key))
+        {
+            prev = it;
+            it = it->next;
+        }
+        if (prev)
+        {
+            prev->next = it->next;
+        }
+        else
+        {
+            table[index] = it->next;
+        }
+    }
+    return 0;
+}
+
+void DeleteChain(Node** head)
+{
+    Node* prev = NULL;
+    while (*head)
+    {
+        prev = (*head);
+        (*head) = (*head)->next;
+        free(prev->data);
+        free(prev);
+    }
+}
+
+void DeleteTable(Node** table)
+{
+    if (!table)
+    {
+        return;
+    }
+    for (int i = 0; i < TABLE_SIZE; i++)
+    {
+        if ((table[i]))
+        {
+            DeleteChain(&table[i]);
+        }
+    }
+    free(*table);
+    table = NULL;
+    free(table);
+    return;
+}
+
+int StartProcess()
+{
+    char command;
+    Node* table[TABLE_SIZE] = { NULL };
+    char buf[BUF_SIZE] = { 0 };
+    char value[INPUT_STR_SIZE] = { 0 };
+    while (fgets(buf, BUF_SIZE, stdin))
+    {
+        sscanf(buf, "%c %s", &command, &value);
+        switch (command)
+        {
+        case 'a':
+            Insert(table, value);
+            break;
+        case 'r':
+            Delete(table, value);
+            break;
+        case 'f':
+            if (LookUp(table, value))
+            {
+                printf("%s", "yes\n");
+            }
+            else
+            {
+                printf("%s", "no\n");
+            }
+            break;
+        default:
+            DeleteTable(table);
+            return 1;
+        }
+    }
+    DeleteTable(table);
+    return 0;
+}
